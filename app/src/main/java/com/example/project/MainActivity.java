e9017d5d80c989cc6a1a9d688ea5ce0bc358b7ce
mizu0016 <mizu.nagnag@gmail.com>
2026-04-25 16:53:39 +0900

    Update MainActivity.java

--- app/src/main/java/com/example/project/MainActivity.java
+++ app/src/main/java/com/example/project/MainActivity.java
@@ -1,20 +1,49 @@
-package com.example.project;
-// Androidx/Appcompat
-import androidx.appcompat.app.AppCompatActivity;
-// Android imports
-import android.os.Bundle;
-import android.widget.TextView;
-// B2 SDK
-import com.backblaze.b2.client.B2Sdk;
+<?xml version="1.0" encoding="utf-8"?>
+<LinearLayout xmlns:android="http://schemas.android.com/apk/res/android"
+    android:layout_width="match_parent"
+    android:layout_height="match_parent"
+    android:orientation="vertical"
+    android:gravity="center"
+    android:padding="20dp">
 
-public class MainActivity extends AppCompatActivity {
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+    <TextView
+        android:id="@+id/titleText"
+        android:layout_width="wrap_content"
+        android:layout_height="wrap_content"
+        android:text="目の健康を守るアプリ"
+        android:textSize="24sp"
+        android:textStyle="bold"
+        android:layout_marginBottom="30dp"/>
 
-        // Setting the TextView in the main layout to the B2SDK version
-        TextView tv = (TextView) findViewById(R.id.main_text);
-        tv.setText(B2Sdk.getVersion());
-    }
-}
+    <Button
+        android:id="@+id/btnStart"
+        android:layout_width="wrap_content"
+        android:layout_height="wrap_content"
+        android:text="通知と重ねて表示の許可を確認する"
+        android:padding="15dp"/>
+
+</LinearLayout><?xml version="1.0" encoding="utf-8"?>
+<LinearLayout xmlns:android="http://schemas.android.com/apk/res/android"
+    android:layout_width="match_parent"
+    android:layout_height="match_parent"
+    android:orientation="vertical"
+    android:gravity="center"
+    android:padding="20dp">
+
+    <TextView
+        android:id="@+id/titleText"
+        android:layout_width="wrap_content"
+        android:layout_height="wrap_content"
+        android:text="目の健康を守るアプリ"
+        android:textSize="24sp"
+        android:textStyle="bold"
+        android:layout_marginBottom="30dp"/>
+
+    <Button
+        android:id="@+id/btnStart"
+        android:layout_width="wrap_content"
+        android:layout_height="wrap_content"
+        android:text="通知と重ねて表示の許可を確認する"
+        android:padding="15dp"/>
+
+</LinearLayout>
